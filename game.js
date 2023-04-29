624c82a0cc26ff025cef87910dce3c597bcc22d2
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-04-29 20:29:58 +0100

    colisão + som de colisão

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+const som_HIT = new Audio()
+som_HIT.src = './efeitos/hit.wav'
+
 const sprites = new Image()
 sprites.src = './sprites.png'
 
@@ -60,19 +63,47 @@ const chao = {
         );
     },
 };
-  
-const flappyBird = {
+
+function fazColisao(flappyBird, chao) {
+  const flappyBirdY = flappyBird.y + flappyBird.altura
+  const chaoY = chao.y
+
+  if(flappyBirdY >= chaoY) {
+    return true
+  }
+
+  return false
+}
+
+function criaFlappyBird() {
+
+  const flappyBird = {
     spriteX: 0,
     spriteY: 0,
     largura: 33,
     altura: 24,
     x: 10,
     y: 50,
+    pulo: 4.6,
+    pula() {
+      flappyBird.velocidade = -flappyBird.pulo
+    },
     gravidade: 0.25,
     velocidade: 0,
     atualiza() {
-        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
-        flappyBird.y = flappyBird.y + flappyBird.velocidade
+  
+      if (fazColisao(flappyBird, chao)) {
+        som_HIT.play()
+
+        setTimeout(() => {
+          mudaParaTela(Telas.INICIO)
+        }, 500)
+        
+        return
+      }
+  
+      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
+      flappyBird.y = flappyBird.y + flappyBird.velocidade
     },
     desenha() {
       contexto.drawImage(
@@ -83,7 +114,13 @@ const flappyBird = {
         flappyBird.largura, flappyBird.altura,
       );
     }
+  }
+
+  return flappyBird
+
 }
+  
+
 
 const mensagemGetReady = {
   spriteX: 134,
@@ -104,18 +141,26 @@ const mensagemGetReady = {
 }
 
 //telas
-
+const globais = {}
 let telaAtiva = {}
 function mudaParaTela(novaTela) {
   telaAtiva = novaTela
+
+  if(telaAtiva.inicializa) {
+    telaAtiva.inicializa()
+  }
+  
 }
 
 const Telas = {
   INICIO: {
+    inicializa() {
+      globais.flappyBird = criaFlappyBird()
+    },
     desenha() {
       planoDeFundo.desenha()
       chao.desenha()
-      flappyBird.desenha()
+      globais.flappyBird.desenha()
       mensagemGetReady.desenha()
     },
     click() {
@@ -132,10 +177,13 @@ Telas.JOGO = {
   desenha() {
     planoDeFundo.desenha()
     chao.desenha()
-    flappyBird.desenha()
+    globais.flappyBird.desenha()
+  },
+  click() {
+    globais.flappyBird.pula()
   },
   atualiza() {
-    flappyBird.atualiza()
+    globais.flappyBird.atualiza()
   }
 }
 
